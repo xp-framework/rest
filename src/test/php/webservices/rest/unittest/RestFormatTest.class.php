7650efd4252900ccac55e111e39f3a2cf96b71f8
Timm Friebe <thekid@thekid.de>
2014-10-29 22:42:30 +0100

    Use iterator_to_array()

--- src/test/php/webservices/rest/unittest/RestFormatTest.class.php
+++ src/test/php/webservices/rest/unittest/RestFormatTest.class.php
@@ -3,149 +3,88 @@
 use unittest\TestCase;
 use webservices\rest\RestFormat;
 use webservices\rest\RestXmlMap;
+use webservices\rest\Payload;
 use io\streams\MemoryInputStream;
 use io\streams\MemoryOutputStream;
-
+use lang\MapType;
 
 /**
- * Test default router
+ * Test RestFormat class
  *
  * @see  xp://webservices.rest.RestFormat
  */
 class RestFormatTest extends TestCase {
 
-  /**
-   * Test JSON
-   * 
-   */
   #[@test]
   public function json_serialize() {
     $res= new MemoryOutputStream();
-    RestFormat::$JSON->write($res, new \webservices\rest\Payload(array('name' => 'Timm')));
+    RestFormat::$JSON->write($res, new Payload(array('name' => 'Timm')));
     $this->assertEquals('{ "name" : "Timm" }', $res->getBytes());
   }
 
-  /**
-   * Test JSON
-   * 
-   */
   #[@test]
   public function json_deserialize() {
     $req= new MemoryInputStream('{ "name" : "Timm" }');
-    $v= RestFormat::$JSON->read($req, \lang\MapType::forName('[:string]'));
-    $this->assertEquals(array('name' => 'Timm'), $v); 
+    $v= RestFormat::$JSON->read($req, MapType::forName('[:string]'));
+    $this->assertEquals(['name' => 'Timm'], $v); 
   }
 
-  /**
-   * Test XML
-   * 
-   */
   #[@test]
   public function xml_serialize() {
     $res= new MemoryOutputStream();
-    RestFormat::$XML->write($res, new \webservices\rest\Payload(array('name' => 'Timm')));
+    RestFormat::$XML->write($res, new Payload(array('name' => 'Timm')));
     $this->assertEquals(
       '<?xml version="1.0" encoding="UTF-8"?>'."\n".'<root><name>Timm</name></root>', 
       $res->getBytes()
     );
   }
 
-  /**
-   * Flattens a RestXmlMap into a map
-   *
-   * @param  webservices.rest.RestXmlMap $in
-   * @return [:var]
-   */
-  protected function flatten(RestXmlMap $in) {
-    $result= array();
-    foreach ($in as $key => $value) {
-      $result[$key]= $value;
-    }
-    return $result;
-  }
-
-  /**
-   * Test XML
-   * 
-   */
   #[@test]
   public function xml_deserialize() {
     $req= new MemoryInputStream('<?xml version="1.0" encoding="UTF-8"?>'."\n".'<root><name>Timm</name></root>');
-    $v= RestFormat::$XML->read($req, \lang\MapType::forName('[:string]'));
-    $this->assertEquals(array('name' => 'Timm'), $this->flatten($v));
+    $v= RestFormat::$XML->read($req, MapType::forName('[:string]'));
+    $this->assertEquals(array('name' => 'Timm'), iterator_to_array($v));
   }
 
-  /**
-   * Test XML
-   * 
-   */
   #[@test]
   public function xml_deserialize_without_xml_declaration() {
     $req= new MemoryInputStream('<root><name>Timm</name></root>');
-    $v= RestFormat::$XML->read($req, \lang\MapType::forName('[:string]'));
-    $this->assertEquals(array('name' => 'Timm'), $this->flatten($v)); 
+    $v= RestFormat::$XML->read($req, MapType::forName('[:string]'));
+    $this->assertEquals(array('name' => 'Timm'), iterator_to_array($v)); 
   }
 
-  /**
-   * Test FORM
-   *
-   */
   #[@test]
   public function form_deserialize() {
     $req= new MemoryInputStream('name=Timm');
-    $v= RestFormat::$FORM->read($req, \lang\MapType::forName('[:string]'));
+    $v= RestFormat::$FORM->read($req, MapType::forName('[:string]'));
     $this->assertEquals(array('name' => 'Timm'), $v);
   }
 
-  /**
-   * Test forMediaType()
-   *
-   */
   #[@test]
   public function application_x_www_form_urlencoded_mediatype() {
     $this->assertEquals(RestFormat::$FORM, RestFormat::forMediaType('application/x-www-form-urlencoded'));
   }
 
-  /**
-   * Test forMediaType()
-   *
-   */
   #[@test]
   public function text_json_mediatype() {
     $this->assertEquals(RestFormat::$JSON, RestFormat::forMediaType('text/json'));
   }
 
-  /**
-   * Test forMediaType()
-   *
-   */
   #[@test]
   public function application_json_mediatype() {
     $this->assertEquals(RestFormat::$JSON, RestFormat::forMediaType('application/json'));
   }
 
-  /**
-   * Test forMediaType()
-   *
-   */
   #[@test]
   public function text_xml_mediatype() {
     $this->assertEquals(RestFormat::$XML, RestFormat::forMediaType('text/xml'));
   }
 
-  /**
-   * Test forMediaType()
-   *
-   */
   #[@test]
   public function application_xml_mediatype() {
     $this->assertEquals(RestFormat::$XML, RestFormat::forMediaType('application/xml'));
   }
 
-  /**
-   * Test forMediaType()
-   *
-   */
   #[@test]
   public function application_octet_stream_mediatype() {
     $this->assertEquals(RestFormat::$UNKNOWN, RestFormat::forMediaType('application/octet-stream'));
