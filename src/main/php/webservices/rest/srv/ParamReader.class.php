48d1f97e246002fd840bbe5f3c68c90f9d55ac68
Timm Friebe <thekid@thekid.de>
2015-07-19 14:06:10 +0200

    QA: Coding standards adjustments

--- src/main/php/webservices/rest/srv/ParamReader.class.php
+++ src/main/php/webservices/rest/srv/ParamReader.class.php
@@ -2,47 +2,50 @@
 
 use webservices\rest\RestFormat;
 use webservices\rest\RestDeserializer;
+use lang\IllegalArgumentException;
 
 /**
  * Reads request parameters
+ *
+ * @test  xp://webservices.rest.unittest.srv.ParamReaderTest
  */
 abstract class ParamReader extends \lang\Enum {
-  protected static $sources= array();
+  private static $sources= [];
   public static $COOKIE, $HEADER, $PARAM, $PATH, $BODY, $REQUEST;
 
   static function __static() {
-    self::$sources['cookie']= self::$COOKIE= newinstance(__CLASS__, array(1, 'cookie'), '{
+    self::$sources['cookie']= self::$COOKIE= newinstance(__CLASS__, [1, 'cookie'], '{
       static function __static() { }
       public function read($name, $target, $request) {
         if (null === ($cookie= $request->getCookie($name, null))) return null;
         return $cookie->getValue();
       }
     }');
-    self::$sources['header']= self::$HEADER= newinstance(__CLASS__, array(2, 'header'), '{
+    self::$sources['header']= self::$HEADER= newinstance(__CLASS__, [2, 'header'], '{
       static function __static() { }
       public function read($name, $target, $request) {
         return $request->getHeader($name, null);
       }
     }');
-    self::$sources['param']= self::$PARAM= newinstance(__CLASS__, array(3, 'param'), '{
+    self::$sources['param']= self::$PARAM= newinstance(__CLASS__, [3, 'param'], '{
       static function __static() { }
       public function read($name, $target, $request) {
         return $request->getParam($name, null);
       }
     }');
-    self::$sources['path']= self::$PATH= newinstance(__CLASS__, array(4, 'path'), '{
+    self::$sources['path']= self::$PATH= newinstance(__CLASS__, [4, 'path'], '{
       static function __static() { }
       public function read($name, $target, $request) {
         return isset($target["segments"][$name]) ? rawurldecode($target["segments"][$name]) : null;
       }
     }');
-    self::$sources['body']= self::$BODY= newinstance(__CLASS__, array(5, 'body'), '{
+    self::$sources['body']= self::$BODY= newinstance(__CLASS__, [5, 'body'], '{
       static function __static() { }
       public function read($name, $target, $request) {
         return \webservices\rest\RestFormat::forMediaType($target["input"])->read($request->getInputStream(), \lang\Type::$VAR); 
       }
     }');
-    self::$sources['request']= self::$REQUEST= newinstance(__CLASS__, array(6, 'request'), '{
+    self::$sources['request']= self::$REQUEST= newinstance(__CLASS__, [6, 'request'], '{
       static function __static() { }
       public function read($name, $target, $request) {
         return $request;
@@ -55,12 +58,13 @@ abstract class ParamReader extends \lang\Enum {
    *
    * @param  string name
    * @return self
+   * @throws lang.IllegalArgumentException
    */
   public static function forName($name) {
     if (isset(self::$sources[$name])) {
       return self::$sources[$name];
     }
-    throw new \lang\IllegalArgumentException('Invalid parameter source "'.$name.'"');
+    throw new IllegalArgumentException('Invalid parameter source "'.$name.'"');
   }
 
   /**
