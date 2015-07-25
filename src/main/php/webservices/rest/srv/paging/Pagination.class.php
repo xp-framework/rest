72f51b9cfef7cc815064aeae597aefdbd0a40811
Timm Friebe <thekid@thekid.de>
2015-07-25 12:12:27 +0200

    Add test for Pagination class

--- src/main/php/webservices/rest/srv/paging/Pagination.class.php
+++ src/main/php/webservices/rest/srv/paging/Pagination.class.php
@@ -2,9 +2,23 @@
 
 use scriptlet\Request;
 
+/**
+ * A pagination instance holds the paging behavior, the request and the
+ * page size. It is created by passing a request instance to the Paging
+ * class' `on()` method.
+ *
+ * @test  xp://webservices.rest.unittest.srv.paging.PaginationTest
+ */
 class Pagination extends \lang\Object {
   private $request, $size, $behavior;
 
+  /**
+   * Creates a new pagination instance
+   *
+   * @param  scriptlet.Request $request
+   * @param  webservices.rest.srv.paging.Behavior $behavior
+   * @param  int $size
+   */
   public function __construct(Request $request, $behavior, $size) {
     $this->request= $request;
     $this->size= $size;
@@ -23,14 +37,14 @@ class Pagination extends \lang\Object {
    *
    * @return int
    */
-  public function page() { return $this->behavior->page($this->request) ?: 1; }
+  public function page() { return (int)($this->behavior->page($this->request) ?: 1); }
 
   /**
    * Returns the limit passed in the request's "Per Page" paramenter, or the default limit of omitted
    *
    * @return int
    */
-  public function limit() { return $this->behavior->limit($this->request) ?: $this->size; }
+  public function limit() { return (int)($this->behavior->limit($this->request) ?: $this->size); }
 
   /**
    * Returns the starting offset
@@ -58,8 +72,11 @@ class Pagination extends \lang\Object {
    * @return webservices.rest.srv.Response
    */
   public function paginate($response, array $elements) {
-    $last= sizeof($elements) <= $this->limit();
-    $last || array_pop($elements);
+    $limit= $this->limit();
+    $last= sizeof($elements) <= $limit;
+    while (sizeof($elements) > $limit) {
+      array_pop($elements);
+    }
     return $this->behavior->paginate($this->request, $response, $last)->withPayload($elements);
   }
 
---
+++ src/test/php/webservices/rest/unittest/srv/paging/PaginationTest.class.php
@@ -0,0 +1,108 @@
+<?php namespace webservices\rest\unittest\srv\paging;
+
+use peer\URL;
+use webservices\rest\srv\Response;
+use webservices\rest\srv\paging\Pagination;
+use webservices\rest\srv\paging\UrlParameters;
+use scriptlet\HttpScriptletRequest;
+
+class PaginationTest extends \unittest\TestCase {
+  const SIZE = 50;
+
+  /**
+   * Creates a new request instance
+   *
+   * @param  string $queryString
+   * @return webservices.rest.srv.paging.Pagination
+   */
+  protected function newFixture($queryString= '') {
+    parse_str(ltrim($queryString, '?'), $params);
+
+    $r= new HttpScriptletRequest();
+    $r->setParams($params);
+    $r->setURI(new URL('http://example.com/'.$queryString));
+
+    return new Pagination($r, new UrlParameters('page', 'per_page'), self::SIZE);
+  }
+
+  #[@test]
+  public function can_create() {
+    $this->newFixture();
+  }
+
+  #[@test]
+  public function size() {
+    $this->assertEquals(self::SIZE, $this->newFixture()->size());
+  }
+
+  #[@test]
+  public function page_defaults_to_1() {
+    $this->assertEquals(1, $this->newFixture()->page());
+  }
+
+  #[@test]
+  public function page_explicitely_given() {
+    $this->assertEquals(2, $this->newFixture('?page=2')->page());
+  }
+
+  #[@test]
+  public function limit_defaults_to_size() {
+    $this->assertEquals(self::SIZE, $this->newFixture()->limit());
+  }
+
+  #[@test]
+  public function limit_explicitely_given() {
+    $this->assertEquals(10, $this->newFixture('?per_page=10')->limit());
+  }
+
+  #[@test, @values([
+  #  ['', 0],
+  #  ['?page=1', 0],
+  #  ['?page=2', self::SIZE],
+  #  ['?page=1&per_page=10', 0],
+  #  ['?page=2&per_page=10', 10]
+  #])]
+  public function start($queryString, $offset) {
+    $this->assertEquals($offset, $this->newFixture($queryString)->start());
+  }
+
+  #[@test, @values([
+  #  ['', self::SIZE],
+  #  ['?per_page=10', 10],
+  #  ['?page=1&per_page=10', 10],
+  #  ['?page=2&per_page=10', 20]
+  #])]
+  public function end($queryString, $offset) {
+    $this->assertEquals($offset, $this->newFixture($queryString)->end());
+  }
+
+  #[@test]
+  public function paginate_on_empty() {
+    $this->assertEquals([], $this->newFixture()->paginate(Response::ok(), [])->payload->value);
+  }
+
+  #[@test, @values([1, 2, self::SIZE])]
+  public function paginate($size) {
+    $elements= array_fill(0, $size, 'element');
+    $this->assertEquals(
+      $elements,
+      $this->newFixture()->paginate(Response::ok(), $elements)->payload->value
+    );
+  }
+
+  #[@test, @values([1, 2, self::SIZE])]
+  public function paginate_removes_excess_elements_from_payload($by) {
+    $this->assertEquals(
+      array_fill(0, self::SIZE, 'element'),
+      $this->newFixture()->paginate(Response::ok(), array_fill(0, self::SIZE + $by, 'element'))->payload->value
+    );
+  }
+
+  #[@test]
+  public function string_representation() {
+    $this->assertEquals(
+      'webservices.rest.srv.paging.Pagination@(page= 1, limit= 50)',
+      $this->newFixture()->toString()
+    );
+  }
+}
