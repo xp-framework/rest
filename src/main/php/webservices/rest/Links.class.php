0ca26a3771506ab37cfa8ce261293f50959ca427
Timm Friebe <thekid@thekid.de>
2016-09-24 20:45:04 +0200

    Link header support

---
+++ src/main/php/webservices/rest/Links.class.php
@@ -0,0 +1,110 @@
+<?php namespace webservices\rest;
+
+use text\StringTokenizer;
+use lang\FormatException;
+
+/**
+ * Link header
+ *
+ * @test xp://webservices.rest.unittest.LinksTest
+ * @see  https://www.w3.org/wiki/LinkHeader
+ * @see  https://tools.ietf.org/html/rfc5988 Web Linking
+ */
+class Links {
+  private $links= [];
+
+  /**
+   * Parser helper function
+   *
+   * @param  text.Tokenizer $st
+   * @param  string $tokens
+   * @return string
+   * @throws lang.FormatException
+   */
+  private function expect($st, $tokens) {
+    $parsed= $st->nextToken($tokens);
+    if (false === strpos($tokens, $parsed)) {
+      throw new FormatException('Expected ['.$tokens.'], have '.\xp::stringOf($parsed));
+    }
+    return $parsed;
+  }
+
+  /**
+   * Parses a Link header into a links structure
+   *
+   * @param  string $header
+   * @throws lang.FormatException If the header is malformed
+   */
+  public function __construct($header) {
+    $st= new StringTokenizer($header, '<>', true);
+    do {
+      $this->expect($st, '<');
+      $uri= $st->nextToken('>');
+      $this->expect($st, '>');
+
+      $params= [];
+      do {
+        if (',' === $this->expect($st, ';,')) break;
+
+        $param= ltrim($st->nextToken('='));
+        $this->expect($st, '=');
+        if ('"' === ($value= $st->nextToken('";,'))) {
+          $value= $st->nextToken('"');
+          $this->expect($st, '"');
+        }
+        $params[$param]= $value;
+      } while ($st->hasMoreTokens());
+
+      $this->links[]= new Link($uri, $params);
+    } while ($st->nextToken('<'));
+  }
+
+  /**
+   * Returns a map of link URIs to parameters, optionally restricted to a given search
+   *
+   * @param  [:string] $search If omitted, all links are returned
+   * @return iterable
+   */
+  public function all($search= null) {
+    if (null === $search) {
+      foreach ($this->links as $link) {
+        yield $link;
+      }
+    } else {
+      foreach ($this->links as $link) {
+        foreach ($search as $param => $compare) {
+          if (!$link->hasParam($param) || (null !== $compare && $compare !== $link->param($param))) continue 2;
+        }
+        yield $link;
+      }
+    }
+  }
+
+  /**
+   * Searches for the first link URI by a given search
+   *
+   * @param  [:string] $search
+   * @param  string $default
+   * @return string
+   */
+  public function uri($search, $default= null) {
+    foreach ($this->all($search) as $link) {
+      return $link->uri();
+    }
+    return $default;
+  }
+
+  /**
+   * Creates a lookup map to a URI by a given link parameter
+   *
+   * @param  string $param
+   * @return [:string]
+   */
+  public function map($param) {
+    $map= [];
+    foreach ($this->links as $link) {
+      if ($link->hasParam($param)) $map[$link->param($param)]= $link->uri();
+    }
+    return $map;
+  }
+}
---
+++ src/test/php/webservices/rest/unittest/LinksTest.class.php
@@ -0,0 +1,83 @@
+<?php namespace webservices\rest\unittest;
+
+use webservices\rest\Link;
+use webservices\rest\Links;
+use lang\FormatException;
+
+class LinksTest extends \unittest\TestCase {
+
+  #[@test, @values([
+  #  ['<http://example.com/?page=2>;rel="next"'],
+  #  ['<http://example.com/?page=2>; rel="next"'],
+  #  ['<http://example.com/?page=2>; rel="next"; hreflang=de'],
+  #  ['<http://example.com/?page=3>; rel="next", <http://example.com/?page=1>; rel="prev"'],
+  #  ['<http://example.com/?page=1>; rel="next"; title="next chapter"'],
+  #  ['<http://example.com/?page=1>; rel="next"; title="a;b"'],
+  #  ['<http://example.com/?page=1>; rel="next"; title="a,b"']
+  #])]
+  public function can_create($header) {
+    new Links($header);
+  }
+
+  #[@test, @expect(FormatException::class), @values([
+  #  [''],
+  #  ['<>'],
+  #  ['<http://example.com/?page=2'],
+  #  ['<http://example.com/?page=2>; rel'],
+  #  ['<http://example.com/?page=2>; rel="next']
+  #])]
+  public function malformed($header) {
+    new Links($header);
+  }
+
+  #[@test]
+  public function all() {
+    $links= new Links('<http://example.com/?page=2>; rel="next"');
+    $this->assertEquals([new Link('http://example.com/?page=2', ['rel' => 'next'])], iterator_to_array($links->all()));
+  }
+
+  #[@test]
+  public function all_with_rel() {
+    $links= new Links('<http://example.com/?page=2>; rel="next", <http://example.com/>; title="Home"');
+    $this->assertEquals([new Link('http://example.com/?page=2', ['rel' => 'next'])], iterator_to_array($links->all(['rel' => null])));
+  }
+
+  #[@test]
+  public function uri_by_rel() {
+    $links= new Links('<http://example.com/?page=3>; rel="next", <http://example.com/?page=1>; rel="prev"');
+    $this->assertEquals(
+      ['http://example.com/?page=1', 'http://example.com/?page=3'],
+      [$links->uri(['rel' => 'prev']), $links->uri(['rel' => 'next'])]
+    );
+  }
+
+  #[@test]
+  public function uri_by_non_existant_rel_returns_null() {
+    $links= new Links('<http://example.com/?page=2>; rel="next"');
+    $this->assertEquals(null, $links->uri(['rel' => 'prev']));
+  }
+
+  #[@test]
+  public function uri_by_non_existant_rel_returns_default() {
+    $links= new Links('<http://example.com/?page=2>; rel="next"');
+    $this->assertEquals('http://example.com', $links->uri(['rel' => 'prev'], 'http://example.com'));
+  }
+
+  #[@test]
+  public function mapping_by_rel() {
+    $links= new Links('<http://example.com/?page=3>; rel="next", <http://example.com/?page=3>; rel="last", <http://example.com/?page=1>; rel="prev"');
+    $this->assertEquals(
+      ['next' => 'http://example.com/?page=3', 'last' => 'http://example.com/?page=3', 'prev' => 'http://example.com/?page=1'],
+      $links->map('rel')
+    );
+  }
+
+  #[@test]
+  public function mapping_by_title_excludes_links_without_title() {
+    $links= new Links('<http://example.com/?page=3>; rel="next", <http://example.com/>; title="Home"');
+    $this->assertEquals(
+      ['Home' => 'http://example.com/'],
+      $links->map('title')
+    );
+  }
+}
